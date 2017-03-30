1088c5a143d034aa6a95384d3e8d43046bbbff42
Animesh <animesh@investmytalent.com>
2017-03-30 22:52:26 +1100

    - reult page finalized

--- www/js/controllers/resultModal.controller.js
+++ www/js/controllers/resultModal.controller.js
@@ -4,7 +4,17 @@ var resultModal = function($scope, resultFactory, $state){
 
   vm.timeNow = new Date();
 
+  /*resultFactory.resultData = [{"ID":"446","Question":"The only supply chain partner with access to business data and forecasts of real data for replacement isaaaa:","QuestionText":"","TmpQuestionText":"The only supply chain partner with access to business data and forecasts of real data for replacement is:","OptA":"A. Consumers","OptB":"B. Retailers","OptC":"C. Distributors","OptD":"D. Manufacturers","OptE":"E. Supply chain","Answer":"B","Explanation":"The only supply chain partner with access to business data and forecasts of real data are the retailers.","GroupId":"7","answered":{"answered":true,"optionSelected":"A","isCorrect":false}},{"ID":"446","Question":"The only supply chain partner with access to business data and forecasts of real data for replacement isaaaa:","QuestionText":"","TmpQuestionText":"The only supply chain partner with access to business data and forecasts of real data for replacement is:","OptA":"A. Consumers","OptB":"B. Retailers","OptC":"C. Distributors","OptD":"D. Manufacturers","OptE":"E. Supply chain","Answer":"B","Explanation":"The only supply chain partner with access to business data and forecasts of real data are the retailers.","GroupId":"7","answered":{"answered":true,"optionSelected":"C","isCorrect":false}},{"ID":"445","Question":"The only supply chain partner with access to business data and forecasts of real data for replacement is:","QuestionText":"","TmpQuestionText":"The only supply chain partner with access to business data and forecasts of real data for replacement is:","OptA":"A. Consumers","OptB":"B. Retailers","OptC":"C. Distributors","OptD":"D. Manufacturers","OptE":"E. Supply chain","Answer":"B","Explanation":"The only supply chain partner with access to business data and forecasts of real data are the retailers.","GroupId":"7","answered":{"answered":true,"optionSelected":"C","isCorrect":false}},{"ID":"446","Question":"The only supply chain partner with access to business data and forecasts of real data for replacement isaaaa:","QuestionText":"","TmpQuestionText":"The only supply chain partner with access to business data and forecasts of real data for replacement is:","OptA":"A. Consumers","OptB":"B. Retailers","OptC":"C. Distributors","OptD":"D. Manufacturers","OptE":"E. Supply chain","Answer":"B","Explanation":"The only supply chain partner with access to business data and forecasts of real data are the retailers.","GroupId":"7","answered":{"answered":true,"optionSelected":"C","isCorrect":false}},{"ID":"446","Question":"The only supply chain partner with access to business data and forecasts of real data for replacement isaaaa:","QuestionText":"","TmpQuestionText":"The only supply chain partner with access to business data and forecasts of real data for replacement is:","OptA":"A. Consumers","OptB":"B. Retailers","OptC":"C. Distributors","OptD":"D. Manufacturers","OptE":"E. Supply chain","Answer":"B","Explanation":"The only supply chain partner with access to business data and forecasts of real data are the retailers.","GroupId":"7","answered":{"answered":true,"optionSelected":"C","isCorrect":false}}];*/
+
   vm.results = resultFactory;
+
+  vm.formatNumber = function (number) {
+    if(number < 10){
+      return '0' + number;
+    }
+    return number;
+  };
+
 
   console.log(resultFactory);
 
