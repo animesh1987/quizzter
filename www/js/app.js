61cd09fbffef7578368cda31b09c7b79f44caaf2
Animesh <animesh@investmytalent.com>
2017-04-21 15:38:46 +1000

    - minor changes

--- www/js/app.js
+++ www/js/app.js
@@ -15,11 +15,13 @@ angular.module('quizzter', ['ngMaterial', 'ui.router', 'ionic'])
         // a much nicer keyboard experience.
         cordova.plugins.Keyboard.disableScroll(true);
       }
-      console.log(window.StatusBar);
       if(window.StatusBar) {
         StatusBar.styleDefault();
         StatusBar.styleLightContent();
         StatusBar.backgroundColorByHexString("#a994f0");
+      }
+      if(navigator && navigator.splashscreen){
+        navigator.splashscreen.hide();
       }
     });
   })
